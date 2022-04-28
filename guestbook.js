4cebd785ea6f601ddcd9dbc556c70e861ed97a97
Muhammed Hasan <87802950+muhammedhasann@users.noreply.github.com>
2022-04-28 13:13:30 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://84c7bce9.eu-de.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
